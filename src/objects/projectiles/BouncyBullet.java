46f16b6702a5fda7f0a7986791a283792dec069e
Nonz007x <comandor969@gmail.com>
2023-10-12 12:33:29 +0700

    Adjust projectile

--- src/objects/projectiles/BouncyBullet.java
+++ src/objects/projectiles/BouncyBullet.java
@@ -3,7 +3,6 @@ package objects.projectiles;
 import gamestates.Playing;
 import main.Game;
 import objects.EnemyProjectile;
-import objects.Projectile;
 import utils.LoadSave;
 
 import java.awt.geom.Rectangle2D;
@@ -12,15 +11,16 @@ import java.awt.image.BufferedImage;
 import static utils.Constants.Projectile.DEFAULT_TICKS_TO_LIVE;
 
 public class BouncyBullet extends EnemyProjectile {
+    private int bounceCount = 0;
     private static BufferedImage[] BOUNCY_BULLET;
 
     static {
-        BOUNCY_BULLET = LoadSave.GetImagesFromSpriteSheet("placeholder.png", 10, 10, 10);
+        BOUNCY_BULLET = LoadSave.GetImagesFromSpriteSheet("enemy_bullet.png", 10, 10, 1);
     }
 
     public BouncyBullet(int x, int y, int speed, double directionX, double directionY, int damage) {
         super(x, y, speed, directionX, directionY, damage, DEFAULT_TICKS_TO_LIVE);
-        hitbox = new Rectangle2D.Float(x, y , 10, 10);
+        hitbox = new Rectangle2D.Float(x, y , 16, 16);
         animations = BOUNCY_BULLET;
     }
 
@@ -32,11 +32,17 @@ public class BouncyBullet extends EnemyProjectile {
             setActive(false);
         }
 
+        if (bounceCount >= 2) {
+            setActive(false);
+        }
+
         if (hitbox.x <= 0 || hitbox.x >= collisionTile[0].length * Game.TILE_SIZE) {
+            bounceCount++;
             directionX = -directionX;
         }
 
         if (hitbox.y <= 0 || hitbox.y >= collisionTile.length * Game.TILE_SIZE) {
+            bounceCount++;
             directionY = -directionY;
         }
     }
--- src/objects/projectiles/BuckShot.java
+++ src/objects/projectiles/BuckShot.java
@@ -9,15 +9,15 @@ import java.awt.image.BufferedImage;
 import static utils.Constants.Projectile.BUCKSHOT_DAMAGE;
 
 public class BuckShot extends PlayerProjectile {
-    private static BufferedImage[] BUCKSHOT_IMAGE;
+    private static final BufferedImage[] BUCKSHOT_IMAGE;
 
     static {
-        BUCKSHOT_IMAGE = LoadSave.GetImagesFromSpriteSheet("placeholder.png", 1, 1, 1);
+        BUCKSHOT_IMAGE = LoadSave.GetImagesFromSpriteSheet("player_bullet.png", 10, 10, 1);
     }
 
     public BuckShot(int x, int y, double directionX, double directionY) {
-        super(x, y, 10, directionX, directionY, BUCKSHOT_DAMAGE, 15);
-        hitbox = new Rectangle2D.Float(x, y, 10, 10);
+        super(x, y, 10, directionX, directionY, BUCKSHOT_DAMAGE, 150);
+        hitbox = new Rectangle2D.Float(x, y, 16, 16);
         animations = BUCKSHOT_IMAGE;
     }
 }
