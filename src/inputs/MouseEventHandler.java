44e5f80a61ac20ce17815850027153f28ccc145a
Nonz007x <comandor969@gmail.com>
2023-10-12 12:35:43 +0700

    Remove timer

--- src/inputs/MouseEventHandler.java
+++ src/inputs/MouseEventHandler.java
@@ -3,20 +3,13 @@ package inputs;
 import gamestates.Gamestate;
 import main.GamePanel;
 
-import javax.swing.Timer;
-import java.awt.*;
 import java.awt.event.*;
 
 public class MouseEventHandler implements MouseListener, MouseMotionListener {
     private final GamePanel gamePanel;
-    private final Timer timer;
 
     public MouseEventHandler(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
-        timer = new Timer(100, e -> {
-        });
-        timer.setRepeats(true);
-        timer.start(); // Start the timer
     }
 
     @Override
